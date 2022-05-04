cddbf5b62e6126252b5c5d38d4aa5de2c75952a1
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2022-05-03 21:49:03 -0400

    Added dynamic trivia populating, then altered the structure because it's way too hard work with, then switched it again and went back to redo the whole component structure. hooray

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 
 import React, {useState, useEffect} from 'react';
+import Question from './components/Question';
+import {nanoid} from 'nanoid';
+import {questions} from './data';
+import {answers} from './data';
 
 function App() {
 
-  const [splashScreen, setSplashScreen] = useState(true)
+  const [splashScreen, setSplashScreen] = useState(false)
+  const [quizOver, setQuizOver] = useState(false)
+  const [quizQuestions, setQuizQuestions] = useState([])
+  const [quizAnswers, setAnswers] = useState([])
+  const [isLoaded, setIsLoaded] = useState(true)
 
+  console.log(quizQuestions)
+  console.log(quizAnswers)
+   useEffect(() => {
+     setQuizQuestions(questions)
+     setAnswers(answers)
+    //fetchTriviaData()
+
+   }, [])
 
   function handleSplashClick() {
+
     setSplashScreen(false)
   }
 
-  const BASE_URL = "https://opentdb.com/api.php?amount=5"
+  const BASE_URL = "https://opentdb.com/api.php?amount=5&type=multiple"
 
-  async function getQuestions() {
+  async function fetchTriviaData() {
     const results = await fetch(BASE_URL)
-    console.log(results)
     let data = await results.json()
-    console.log(data)
+    
+    const questionsArr = data.results.map(( {incorrect_answers, correct_answer, question}) => ({incorrect_answers, correct_answer, question}))
+    let formattedAnswers = []
+    let formattedQuestions = []
+    for( let i = 0; i < questionsArr.length; i++){
+      const answers = questionsArr[i].incorrect_answers.map(((item, index) => ({answer: decodeHTMLEntities(item), isCorrect: false, isSelected: false, id: nanoid() })))
+
+      answers.push({
+      answer: decodeHTMLEntities(questionsArr[i].correct_answer),
+      isCorrect: true,
+      isSelected: false,
+      id: nanoid()
+      })
+      answers.sort(() => Math.random() - 0.5)
+      formattedAnswers.push({answers: answers})
+      formattedQuestions.push({question: decodeHTMLEntities(questionsArr[i].question)})
+    }
+    setQuizQuestions(formattedQuestions)
+    setAnswers(formattedAnswers)
+    console.log(formattedQuestions)
+    console.log(formattedAnswers)
+    setIsLoaded(true)
   }
 
   function decodeHTMLEntities(text) {
@@ -25,7 +62,43 @@ function App() {
     return textArea.value;
   }
 
-  // getQuestions()
+  //i am so confused. redoing this, but not now
+  function selectAnswer(id) {
+    const newQuestions = [...quizQuestions]
+    for(let i = 0; i < newQuestions.length; i++){
+      for(let j = 0; j < newQuestions[i].answers.length; j++){
+        console.log(id)
+        console.log(newQuestions[i].answers[j].id)
+        if(newQuestions[i].answers[j].id === id){
+          newQuestions[i].answers[j].isSelected = true
+        } else {
+          newQuestions[i].answers[j].isSelected = false
+        }
+      }
+    }
+    console.log(newQuestions)
+    setQuizQuestions(newQuestions)
+  }
+    // setQuizQuestions((prevQuestions) => {
+    //   return { ...prevQuestions,
+    //   answers: prevQuestions.answers.map((item) => {
+    //     return item.id === id ? {...item, isSelected: true} : item
+    //   }),
+    //   }
+    //})
+
+  
+
+  function scoreQuiz() {
+    setQuizOver(quizOver => true)
+  }
+
+  function resetQuiz() {
+    setQuizOver(quizOver => false)
+  }
+
+
+
   return (
     <div className="App">
       {splashScreen && 
@@ -36,55 +109,15 @@ function App() {
         </div>
       }
  
-      {!splashScreen && <div className="quiz-container">
-        <div className="question-container">
-          <h4 className="question-text">How would one say goodbye in Spanish?</h4>
-          <div className="answers-container">
-            <button className="answer selected">Adios</button>
-            <button className="answer">Hola</button>
-            <button className="answer">Au Revoir</button>
-            <button className="answer">Salir</button>
-          </div>
-        </div>
-        <div className="question-container">
-          <h4 className="question-text">Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?</h4>
-          <div className="answers-container">
-            <button className="answer selected">Cabbge Patch Kids</button>
-            <button className="answer">Transformers</button>
-            <button className="answer">Care Bears</button>
-            <button className="answer">Rubik's Cube</button>
-          </div>
-        </div>
-        <div className="question-container">
-          <h4 className="question-text">How would one say goodbye in Spanish?</h4>
-          <div className="answers-container">
-            <button className="answer selected">Adios</button>
-            <button className="answer">Hola</button>
-            <button className="answer">Au Revoir</button>
-            <button className="answer">Salir</button>
-          </div>
-        </div>
-        <div className="question-container">
-          <h4 className="question-text">How would one say goodbye in Spanish?</h4>
-          <div className="answers-container">
-            <button className="answer selected">Adios</button>
-            <button className="answer">Hola</button>
-            <button className="answer">Au Revoir</button>
-            <button className="answer">Salir</button>
-          </div>
-        </div>
-        <div className="question-container">
-          <h4 className="question-text">How would one say goodbye in Spanish?</h4>
-          <div className="answers-container">
-            <button className="answer correct">Adios</button>
-            <button className="answer incorrect">Hola</button>
-            <button className="answer selected incorrect">Au Revoir</button>
-            <button className="answer selected">Salir</button>
-          </div>
-        </div>
-        <div className="score">
-           <h4 className="correct-answers">You score 3/5 correct answers</h4> 
-          <button className="check-answers">Check Answers</button>
+      {!splashScreen && isLoaded && <div className="quiz-container">
+        {quizQuestions.map((item, index) => 
+          (<Question question={item.question} key={index}  />)
+        )}
+      
+        <div className="score-container">
+          {quizOver && <h4 className="score-answers">You score 3/5 correct answers</h4> }
+          {!quizOver ? <button className="check-answers" onClick={scoreQuiz}>Check Answers</button> :
+            <button className="check-answers" onClick={resetQuiz}>Play Again</button> }
         </div>
       </div>
     }
